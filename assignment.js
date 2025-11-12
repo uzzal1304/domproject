deb17547c4be2ba9c73adb9e5f6f7a5bab4ebbc6
suman roy <gbsumanroy@gmail.com>
2025-11-12 21:53:07 +0600

    seven commit

--- assignment.js
+++ assignment.js
@@ -12,4 +12,9 @@ function addTask(){
     const text = taskinput.value.trim();
     if(text=== "") return;
 
+    const today = new Date();
+    const date = today.toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+    });
    
