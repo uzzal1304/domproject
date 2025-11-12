3033afe67888723b44d866a9939ecd4f919f256b
suman roy <gbsumanroy@gmail.com>
2025-11-12 21:49:18 +0600

    four commit

--- assignment.js
+++ assignment.js
@@ -3,3 +3,5 @@ const addBtn=document.getElementById("addBtn");
 const taskinput=document.getElementById("taskinput");
 const summary=document.getElementById("summary");
 
+let tasks =[];
+addBtn.addEventListener("click", addTask);
