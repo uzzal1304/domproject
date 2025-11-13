2df7fd093e6368bb3790fc1e7411f885494e82bd
suman roy <gbsumanroy@gmail.com>
2025-11-13 11:46:29 +0600

    nine commit

--- assignment.js
+++ assignment.js
@@ -19,6 +19,43 @@ function addTask(){
     });
     const task = {
         id:Date.now(),
+        text,
+        date,
+        completed: false,
+    };
+    tasks.push(task);
+    taskinput.value="";
+    renderTasks();
+}
+function renderTasks(){
+    taskTable.innerHTML="";
+    tasks.forEach((task, index) =>{
+        const row = document.createElement("tr");
+        row.className ="border-b hover:bg-gray-50";
 
-    }
+        row.innerHTML = `<td class="py-2 border">${index + 1}</td>
+        <td class="py-2 border">${task.text}</td>
+        <td class="py-2 border">${task.date}</td>
+        <td class="py-2 border"><input type="checkbox" ${task.completed ? "checked" : ""} class="accent-teal-500">        </td>
+        <td class="py-2 border">
+        <button class="edit-btn text-blue-500 hover:text-blue-700 mr-2">pen</button>    </td>        
+        <button class="delete-btn text-red-500 hover:text-red-700 ">delete</button> </td>`;  
+        
+        row.querySelector("input").addEventListener("change", (e)=>{
+            task.completed = e.target.checked;
+            updateSummary();
+        });
+        row.querySelector(".delete-btn").addEventListener("click",()=>{
+            tasks = tasks.filter((t)=> t.id !==task.id);
+            renderTasks();
+        });
+        row.querySelector(".edit-btn").addEventListener("click",()=>{
+            const newText= prompt("Edit your task:", task.text);
+            if (newText !== null &&newText.trim() !== ""){task.text = newText.trim();
+                renderTasks();
+            }
+        });
+        taskTable.appendChild(row);
+    });
+    updateSummary();
 }
